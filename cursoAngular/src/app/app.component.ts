c0b5fbff3721df290c42d244d94262843143bcf7
Carlosmcr <carlos-mcr@hotmail.com>
2018-11-19 17:50:02 +0100

    multiplicador Pipe

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -12,19 +12,25 @@ export class AppComponent {
       id: 1,
       nombre: 'Angular 2',
       descripcion: 'Introducción al desarrollo avanzado en Js',
-      duracion: 8
+      duracion: 8,
+      precio: 50.20,
+      fecha: new Date(1992, 11, 5)
     },
     {
       id: 2,
       nombre: 'Angular 3',
       descripcion: 'Introducción al desarrollo avanzado en Js',
-      duracion: 5
+      duracion: 5,
+      precio: 950.01,
+      fecha: new Date()
     },
     {
       id: 1,
-      nombre: 'Angular 2',
+      nombre: 'Java',
       descripcion: 'Introducción al desarrollo avanzado en Js',
-      duracion: 10
+      duracion: 10,
+      precio: 150.99,
+      fecha: new Date()
     },
   ];
 

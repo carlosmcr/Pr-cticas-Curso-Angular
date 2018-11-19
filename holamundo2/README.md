17a40b12a66c21109acb5052196373d2eec6c7c3
Carlosmcr <carlos-mcr@hotmail.com>
2018-11-19 12:06:37 +0100

    borrado

--- holamundo2/README.md
+++ holamundo2/README.md
@@ -1,4 +1,4 @@
-# Holamundo
+# Holamundo2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.6.
 
--- holamundo2/src/app/app.component.spec.ts
+++ holamundo2/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'holamundo'`, () => {
+  it(`should have as title 'holamundo2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('holamundo');
+    expect(app.title).toEqual('holamundo2');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to holamundo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to holamundo2!');
   });
 });

2280c7ac7859988d100e41a63e6ff6a809fa88b1
Carlosmcr <carlos-mcr@hotmail.com>
2018-11-19 16:17:47 +0100

    Click

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'cursoAngular';
+  titulo = 'Mis cursos';
+  cursos = [
+    {
+      id: 1,
+      nombre: 'Angular 2',
+      descripcion: 'Introducción al desarrollo avanzado en Js',
+      duracion: 8
+    },
+    {
+      id: 2,
+      nombre: 'Angular 3',
+      descripcion: 'Introducción al desarrollo avanzado en Js',
+      duracion: 5
+    },
+    {
+      id: 1,
+      nombre: 'Angular 2',
+      descripcion: 'Introducción al desarrollo avanzado en Js',
+      duracion: 10
+    },
+  ];
+
+  seleccionar(curso) {
+    console.log(curso);
+  }
 }
